37072395d8310c1575f265dc211fc1dd0b247146
Deep Rahman <dp.rahman@gmail.com>
2022-04-07 10:09:07 +0000

    debug_config modified

--- debug_config.php
+++ debug_config.php
@@ -3,17 +3,15 @@
 $stedb_forms_account = get_option( 'stedb_forms_account', array() );
 if(isset($_SERVER['HTTP_HOST'])){
   $host = substr($_SERVER['HTTP_HOST'], 0, 5);
-  if(in_array($host, array('local', '127.0', '192.1','dev.d','dbm.d'))){
-     define('DEBUG', TRUE);
-     define ('REMOTE', TRUE);
+  if(in_array($host, array('local', '127.0', '192.1','dev.d','dbm.d','wp.dee'))){
+     define ('REMOTE', FALSE);
   }else{
      define('DEBUG', FALSE);
-     define ('REMOTE', FALSE);
   }
 }
 $account = array (
-  'user_id' => '1',
-  'secret' => '123456',
+  'user_id' => REMOTE?'2529':'1',
+  'secret' => REMOTE?'unBBaf28':'123456',
   'base_url' => REMOTE?'https://opt4.stedb.com/dbm9x/api/':'http://dbm.deeprahman.lo/dbm9x/api/',
 );
 
